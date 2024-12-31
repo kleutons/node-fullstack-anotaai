98f06ec92119bde0fbb379f4ff4b11330af43b76
kleuton novais <106082564+kleutons@users.noreply.github.com>
2024-12-31 17:37:23 -0300

    removed service item

--- backend/src/routes/routeDefinition.ts
+++ backend/src/routes/routeDefinition.ts
@@ -1,8 +1,6 @@
 const URL_PREFIX = '/api';
 
 export enum ROUTERS{
-    ITEM            = URL_PREFIX+'/item',
-    ITENS           = URL_PREFIX+'/itens',
     USER            = URL_PREFIX+'/user',
     USERS           = URL_PREFIX+'/users',
 }
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { routesItem } from "./item.routes";
 import { routesUser } from "./user.routes";
 
 const router = Router();
 
 // Call all routes here
-router.use(routesItem); 
 router.use(routesUser); 
 
 export default router;
