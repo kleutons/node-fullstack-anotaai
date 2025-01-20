780ef9dfdb89ec8d14b5dda4e991546d2295762a
kleuton novais <106082564+kleutons@users.noreply.github.com>
2025-01-20 01:04:11 -0300

    refactor: add text and layout page not found

--- frontend/src/app/NotFoundPage.tsx
+++ frontend/src/app/NotFoundPage.tsx
@@ -1,8 +1,23 @@
+import { NavLink } from "react-router";
+import TitlePage from "../components/dashboard/TitlePage";
+import ButtonPrimay from "../components/dashboard/ButtonPrimay";
+
 
 export default function NotFoundPage(){
-    return(
-        <>
-         404 Page not Found!
-        </>
+    return (
+        <div className="w-full h-svh flex flex-col gap-4 justify-center items-center ">
+            <div className="max-w-2xl flex flex-col gap-4 justify-center text-center">
+                <p className="text-blue-500 text-5xl font-bold">404</p>
+        
+                <TitlePage text="Page not found!" />
+
+                <p>Desculpe, não conseguimos encontrar a página que você está procurando.</p>
+
+                
+                <NavLink to="/">
+                    <ButtonPrimay text="Voltar para Página Inicial" />
+                </NavLink>
+            </div>
+        </div>
     )
 }
