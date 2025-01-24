4c04876685c1ae8fd53256035b3d44046fb67ae9
kleuton novais <106082564+kleutons@users.noreply.github.com>
2025-01-23 21:00:14 -0300

    refactor: use axios

--- frontend/src/app/TestPage.tsx
+++ frontend/src/app/TestPage.tsx
@@ -1,47 +1,13 @@
 import ButtonPrimay from "../components/dashboard/ButtonPrimay";
 import TitlePage from "../components/dashboard/TitlePage";
-import useAxios from "../hooks/useAxios";
-import axiosInstance from "../utils/AxiosInstance";
-
-
-
-interface PokemonType{
-    name: string,
-    url: string
-}
-
-
-interface ApiResponse {
-    results: PokemonType[]
-}
 
 export default function TestPage() {
 
-    const { data, loading, error } = useAxios<ApiResponse>({
-        axiosInstance,
-        method: "get",
-        url: "/"
-    })
-
-
-    if (loading) return <div>Loading...</div>; 
-    if (error) return <div>{error}</div>;
-
+ 
     return(
         <>
             <TitlePage text="Test Page" />
             
-            <div>
-                {data?.results?.map((pokemon, index) =>(
-                    <div key={`Pok-${index}`} className="mb-3 p-2 border-b border-gray-300">
-                        <h3 className="text-lg font-semibold">{pokemon.name}</h3>
-                        <p>{pokemon.url}</p>
-                    </div>
-                ))}
-            </div>
-
-            <br />
-
             <ButtonPrimay text="Execultar GET" /> 
         </>
     )
--- frontend/src/hooks/useAxios.tsx
+++ frontend/src/hooks/useAxios.tsx
@@ -1,40 +1,48 @@
 import { AxiosError, AxiosInstance, AxiosResponse } from "axios";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import useAuth from "./useAuth";
 
 export interface useAxiosProps {
   axiosInstance: AxiosInstance;
   method: "get" | "post" | "put" | "delete";  // Métodos HTTP esperados
   url: string;
-  token:string | null;
-  othersConfigs?: object; 
+  formData?: object
 }
 
 interface ErrorResponse {
   error?: string;  
 }
 
-export default function useAxios<T>({ axiosInstance, method, url, token, othersConfigs }: useAxiosProps) {
+export default function useAxios<T>({ axiosInstance, method, url, formData }: useAxiosProps) {
+  
+  const {token} = useAuth();
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
+  
+  const headers = {
+    headers: { 'Authorization': `Bearer ${token}` }
+  }
   // Use useRef to store axiosInstance
   const axiosInstanceRef = useRef(axiosInstance);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      console.log('entrou no use axios');
-      if(token){
-        setError(null);
+
+    const sendRequest = async () => {
+      if(token){        
         setLoading(true);
         try {
-          const response: AxiosResponse<T> = await axiosInstanceRef.current[method](url, othersConfigs);
+          let response: AxiosResponse<T> ;
+          if(formData){
+            response = await axiosInstanceRef.current[method](url, formData, headers);
+          }else{
+            response = await axiosInstanceRef.current[method](url, headers);
+          }
           setData(response.data);
         } catch (err) {
           console.log(err);
           const axiosError = err as AxiosError<ErrorResponse>;
           const returnErro = axiosError.response?.data ? `Error: ${axiosError.response.data?.error}` : `Error fetching data: ${err}`;
-          setError(returnErro);
+          toast.error(returnErro)
         } finally {
           setLoading(false);
         }
@@ -42,8 +50,6 @@ export default function useAxios<T>({ axiosInstance, method, url, token, othersC
       
     };
     
-    fetchData();
-  }, [token] );
 
-  return { data, loading, error };
+  return { sendRequest, response:{ data, loading } };
 }
