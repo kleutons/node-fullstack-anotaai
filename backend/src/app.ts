3e1aba1994a1f32a9d148140c0360694375a3e8f
kleuton novais <106082564+kleutons@users.noreply.github.com>
2025-02-11 20:41:01 -0300

    backend: add allowed origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,17 @@ import express from "express"
 import cors from 'cors';
 import router from "./routes/routes";
 
+const allowedOrigin = process.env.ALLOWED_ORIGIN || '*';
+
+if (!process.env.ALLOWED_ORIGIN) {
+    console.warn('A variável ALLOWED_ORIGIN não está definida no arquivo .env');
+}
+
 export default function createApp(){
     const app = express();
 
     const corsOptions = {
-        origin: '*', // Permite requisições de qualquer origem
+        origin: allowedOrigin, // Permite requisições de qualquer origem
         methods: 'GET,POST,PUT,DELETE', // Métodos HTTP permitidos
         allowedHeaders: 'Content-Type,Authorization' // Cabeçalhos permitidos
     };
